d49b251aef720a4ad5eb5b71128766a81e71e4d3
sooj1n <sujin19371@gmail.com>
2024-07-25 14:43:46 +0900

    add error msg

--- src/sj_args_history/cli.py
+++ src/sj_args_history/cli.py
@@ -29,7 +29,8 @@ def cmd():
             # TODO 특정 날짜의 명령어 TOP N
         else:
             print("TODO - 에러 안내 메시지를 주면")
-            parser.print_help()
+            # parser.print_help()
+            parser.error("-t 옵션은 -d 옵션과 함께 사용하시오!")
     else:
         # TODO - 사용법을 출력한다
         parser.print_help()
